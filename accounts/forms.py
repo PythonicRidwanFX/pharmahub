4fec5f56061ae29587b339bb6e613b23f0a350df
AKINTUNDE RIDWAN DAMILARE <akintunderidwan113@gmail.com>
2026-03-24 12:15:53 +0100

    PharmaHub project setup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
-# ===============================
-# PHARMACY REGISTRATION FORM
-# ===============================
 class PharmacyRegistrationForm(UserCreationForm):
     pharmacy_name = forms.CharField(max_length=255)
     pharmacy_email = forms.EmailField()
@@ -25,52 +22,22 @@ class PharmacyRegistrationForm(UserCreationForm):
             'pharmacy_address',
         ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        # Add basic styling (important)
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
-
-
-# ===============================
-# STAFF CREATE FORM
-# ===============================
 class StaffCreateForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'role', 'password1', 'password2']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
 
 
-# ===============================
-# LOGIN FORM (USERNAME OR EMAIL)
-# ===============================
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label="Username or Email",
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter username or email',
-        })
-    )
-
-    password = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter password',
-        })
-    )
-
-    def confirm_login_allowed(self, user):
-        if not user.is_active:
-            raise forms.ValidationError(
-                "This account is inactive. Please contact support.",
-                code='inactive',
-            )
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Username',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Password',
+    }))
